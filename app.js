237cb73a41af77c2a382f111184354467f9f4b6a
markovecko <marko.vecko@gmail.com>
2016-12-26 20:00:40 -0800

    Uploaded implemented solution to assignment 1

---
+++ app.js
@@ -0,0 +1,55 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheckerApp', [])
+.controller('LunchCheckerController', LunchCheckerController);
+
+LunchCheckerController.$inject = ['$scope'];
+
+function LunchCheckerController($scope) {
+  $scope.messageString = "";
+  $scope.inputString = "";
+  $scope.messageTextColor = "black";
+  $scope.textboxBorderColor = "transparent";
+
+  $scope.countItemsInString = function(){
+    if($scope.inputString.trim().length == 0)
+    {
+      $scope.messageString = "Please enter data first";
+      $scope.messageTextColor = "red";
+    }
+    else
+    {
+      $scope.messageTextColor = "green";
+      var itemNum = countItems($scope.inputString)
+
+      if(itemNum > 3)
+      {
+        $scope.messageString = "Too much!";
+      }
+      else
+      {
+        $scope.messageString = "Enjoy!";
+      }
+    }
+
+    $scope.textboxBorderColor = $scope.messageTextColor;
+};
+}
+
+function countItems (itemsString) {
+  var numOfItems = 0;
+  var arrayOfItems = itemsString.split(',');
+
+  for(var i = 0; i < arrayOfItems.length; i++)
+  {
+    if(arrayOfItems[i].trim().length > 0)
+    {
+      numOfItems++;
+    }
+  }
+
+  return numOfItems;
+};
+
+})();
